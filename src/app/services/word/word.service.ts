65e3c067e8e43e67ca1da079ff6cc4c802aff813
OtavioFSantos <otaviofs01@gmail.com>
2024-09-24 20:57:44 -0300

    feat: add words service

---
+++ src/app/services/word/word.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WordService {
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  getWords(page: number): Observable<any> {
+    const apiUrl: string = "http://localhost/entries/entries.php";
+
+    return this.http.get<any>(`${apiUrl}?page=${page}`);
+  }
+
+  getWordInfo(word: string): Observable<any> {
+    const apiUrl: string = "http://localhost/entries/entry.php";
+    const token = this.authService.getToken();
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any>(`${apiUrl}?word=${word}`, { headers });
+  }
+}
