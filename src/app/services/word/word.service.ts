07d33ef84ab64329b8a112693e562a1f9b989bae
OtavioFSantos <otaviofs01@gmail.com>
2024-09-25 15:17:21 -0300

    feat: add get history, get favorites and fav, unfav

--- src/app/services/word/word.service.ts
+++ src/app/services/word/word.service.ts
@@ -12,10 +12,30 @@ export class WordService {
     private authService: AuthService
   ) { }
 
-  getWords(page: number): Observable<any> {
+  getWords(page: number, search: string): Observable<any> {
     const apiUrl: string = "http://localhost/dictionary-back/entries/entries.php";
 
-    return this.http.get<any>(`${apiUrl}?page=${page}`);
+    return this.http.get<any>(`${apiUrl}?page=${page}&search=${search}`);
+  }
+
+  getHistory(page: number): Observable<any> {
+    const apiUrl: string = "http://localhost/dictionary-back/user/history.php";
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any>(`${apiUrl}?page=${page}`, { headers });
+  }
+
+  getFavorites(page: number): Observable<any> {
+    const apiUrl: string = "http://localhost/dictionary-back/user/favorites.php";
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any>(`${apiUrl}?page=${page}`, { headers });
   }
 
   getWordInfo(word: string): Observable<any> {
@@ -27,5 +47,27 @@ export class WordService {
     });
 
     return this.http.get<any>(`${apiUrl}?word=${word}`, { headers });
+  }
+
+  favoriteWord(word: string): Observable<any> {
+    const apiUrl: string = "http://localhost/dictionary-back/entries/favorite.php";
+    const token = this.authService.getToken();
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any>(`${apiUrl}?word=${word}`, { headers });
+  }
+
+  unfavoriteWord(word: string): Observable<any> {
+    const apiUrl: string = "http://localhost/dictionary-back/entries/unfavorite.php";
+    const token = this.authService.getToken();
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.delete<any>(`${apiUrl}?word=${word}`, { headers });
   }
 }
